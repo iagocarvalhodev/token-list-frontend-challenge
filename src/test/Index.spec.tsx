a003c91d6a9aefc14c89cfb0a5563f2782c81d02
Iago Carvalho <iagocarvalhoweb@gmail.com>
2023-01-16 02:07:40 -0300

    add test

--- src/test/Index.spec.tsx
+++ src/test/Index.spec.tsx
@@ -1,13 +1,25 @@
-import { render } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import WS from 'jest-websocket-mock'
 import React from 'react'
-
-
 import Home from '../../pages/index'
+
+let ws: WS
+beforeEach(() => {
+  ws = new WS('wss://api.foxbit.com.br/')
+})
+afterEach(() => {
+  WS.clean()
+})
 
 describe('The index page', () => {
   it('should wait for the grid to be displayed', () => {
     const { getByTestId, findByText } = render(<Home />)
     expect(getByTestId('grid')).toBeInTheDocument()
     expect(findByText('Foxbit - Frontend Challenge')).toBeTruthy()
+  })
+
+  it('should connect to the WebSocket server', async () => {
+    render(<Home />)
+    expect(ws.connected).toBeTruthy()
   })
 })
